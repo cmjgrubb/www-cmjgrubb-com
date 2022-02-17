e6bc4172d3e949c75ac847b73ea11c6296736c45
cmjgrubb <cgrubb@266a.net>
2022-02-16 22:52:38 -0500

    Reorganized files and started Express refactoring

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
